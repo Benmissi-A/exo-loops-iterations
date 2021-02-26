bdd242d5967cc47463ba2683a3316d89ce134898
Benmissi-A <aminebenmissi@hotmail.fr>
2021-02-26 15:08:16 +0100

    exercises done

---
+++ onlyMultipleOf3And7.js
@@ -0,0 +1,7 @@
+for(let i=0 ; i < 1000 ; ++i){
+    if (i%3 === 0) {
+    console.log(`${i} est multiple de 3`)
+  } else if(i%7 === 0){
+    console.log(`${i} est multiple de 7`)
+  }
+}
---
+++ threeWayCountDown.js
@@ -0,0 +1,20 @@
+let nb = 101
+
+for(let i= nb ; i>0 ; i-=2){
+  console.log(i)
+}
+
+
+let i = nb
+while(i >= 0 ){
+    console.log(i)
+  i -= 2
+}
+
+
+ i = nb
+
+do {
+  console.log(i)
+  i -= 2
+} while( i > 0)
